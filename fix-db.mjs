406f3fca26aa03c1561a75742cbc6bf99bfa64e0
prasanna <prasanna@BSS-011>
2026-06-03 19:22:43 +0530

    chore: remove Windows deployment scripts, documentation, and unused dev dependencies

--- fix-db.mjs
+++
@@ -1,5 +0,0 @@
-import Database from 'better-sqlite3'; 
-const db = new Database('server/app.db'); 
-db.prepare(`UPDATE client_logos SET logo_url = '/assets/clients/Medianet.png' WHERE logo_url LIKE '%Medianet_Maldives.jpg%'`).run(); 
-db.prepare(`UPDATE client_logos SET logo_url = '/assets/clients/RCSC-Bhutan.png' WHERE logo_url LIKE '%RCSC.jpg%'`).run(); 
-console.log('Fixed logos');
